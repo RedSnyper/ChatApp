b643b2b6f227e4b3048dcf46e676aef0119b9109
Jeevan <zbon77@gmail.com>
2019-06-12 23:46:36 +0545

    Extended Thread class

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        }catch (Exception e)
+        {
+            e.getCause();
+        }
         LoginPage loginPage = new LoginPage();
         loginPage.start();
     }
