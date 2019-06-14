f11da557a6963c6d2ac878cdb5ef714c0113ec2a
Jeevan <zbon77@gmail.com>
2019-06-14 23:28:54 +0545

    ServerConnect now connects to a listening server

--- ServerConnect.java
+++ ServerConnect.java
@@ -1,47 +1,51 @@
-import javax.crypto.interfaces.PBEKey;
-import javax.swing.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class ServerConnect extends Thread implements ActionListener {
 
-
+    private String loginType; //either register or login
     private String userName;
     private String regEmailAddress;
     private String gender;
     private char[] password;
     private String loginEmailAddress;
-    private String loginPassword;
-    private ServerSocket serverSocket;
+    private char[] loginPassword;
     private Socket socket;
 
 
         //program to send the database to server
-    public ServerConnect(String userName, String email, String gender, char[] password)
+    public ServerConnect(String loginType,String userName, String email, String gender, char[] password)
     {
+        this.loginType = loginType;
         this.userName = userName;
         this.regEmailAddress = email;
         this.gender = gender;
         this.password = password;
     }
-
+    public ServerConnect(String loginType,String loginEmailAddress,char[] loginPassword)
+    {
+        this.loginType = loginType;
+        this.loginEmailAddress = loginEmailAddress;
+        this.loginPassword = loginPassword;
+    }
     public void run()
     {
         System.out.println(userName);
         System.out.println(regEmailAddress);
         System.out.println(gender);
-
+        ObjectOutputStream objectOutputStream = null;
         try{
             socket = new Socket("locahost",5000);
-
-
-
-
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            if(loginType.equals("register")) {
+                objectOutputStream.writeObject(new ServerConnect(loginType, userName, regEmailAddress, gender, password));
+            }else{
+                objectOutputStream.writeObject(new ServerConnect(loginType,loginEmailAddress,loginPassword));
+            }
         }catch(IOException e)
         {
             System.out.println();
@@ -49,9 +53,6 @@ public class ServerConnect extends Thread implements ActionListener {
 
 
     }
-
-
-
 
     @Override
     public void actionPerformed (ActionEvent e){
